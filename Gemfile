4b0f21b60e506aed184cc6b63e624a10b65ed202
Courtney Miller <courtneymiller919@gmail.com>
2017-07-08 17:11:41 -0600

    3 update

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 gemspec
 
 gem 'httparty', '~> 0.13'
+gem 'json', '~> 2.1', '>= 2.1.0'
--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
@@ -11,5 +12,10 @@ class Kele
     response = self.class.post('/sessions', body: { email: @email, password: @password })
     raise "Invalid email or password. Try again?" if response.code == 404
     @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get('/users/me', headers: { :authorization => @auth_token, :content_type => 'application/json' })
+    @user_info = JSON.parse(response.body)
   end
 end
