cf6c9ac88723130039fbb4f2570a10c0b7126a22
Courtney Miller <courtneymiller919@gmail.com>
2017-07-08 15:57:37 -0600

    2 update

---
+++ lib/kele.rb
@@ -0,0 +1,15 @@
+require 'httparty'
+
+class Kele
+  include HTTParty
+  base_uri "https://www.bloc.io/api/v1/"
+
+  def initialize(email, password)
+    @email = email
+    @password = password
+
+    response = self.class.post('/sessions', body: { email: @email, password: @password })
+    raise "Invalid email or password. Try again?" if response.code == 404
+
+  end
+end
