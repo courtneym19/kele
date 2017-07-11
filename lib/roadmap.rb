e4a4b856d02d9e779a07baadca4fe1b29c1fe6ed
Courtney Miller <courtneymiller919@gmail.com>
2017-07-11 11:34:53 -0600

    5 complete

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -3,7 +3,6 @@ module Roadmap
   def get_roadmap(roadmap_id)
     response = self.class.get("/roadmaps/#{roadmap_id}", headers: { "authorization": @auth_token } )
     @roadmap = JSON.parse(response.body)
-    p @roadmap
   end
 
   def get_checkpoint(checkpoint_id)
